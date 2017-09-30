d17e09a9082d42d913011b8d80d5462719414695
Sajfodias <reigenhurt@gmail.com>
2017-09-30 17:15:32 +0200

    Probably fix for OutOfRangeException in Normalization - maybe error in indexes.

--- Wyszukiwarka_publikacji_v0.2/Logic/ClusteringAlgorithms/Used functions/Normalization.cs
+++ Wyszukiwarka_publikacji_v0.2/Logic/ClusteringAlgorithms/Used functions/Normalization.cs
@@ -13,16 +13,16 @@ namespace Wyszukiwarka_publikacji_v0._2.Logic.ClusteringAlgorithms.Used_function
             List<string> Term_Collection = CreateTermCollection.GenerateTermCollection();
             List<DocumentVector> docCollection = documentCollection;
 
-            for(int i=0; i<=Term_Collection.Count()-1; i++)
+            for(int i=0; i<=Term_Collection.Count(); i++)
             {
                 float max = 0;
-                for(int j=0; j<=docCollection.Count()-1; j++)
+                for(int j=0; j<=docCollection.Count(); j++)
                 {
                     if (docCollection[i].VectorSpace[j] >= max)
                         max = docCollection[i].VectorSpace[j];
                 }
 
-                for(int k=0; k <=docCollection.Count()-1; k++)
+                for(int k=0; k <=docCollection.Count(); k++)
                 {
                     if (max > 0)
                         docCollection[i].VectorSpace[k] = docCollection[i].VectorSpace[k] / max;
